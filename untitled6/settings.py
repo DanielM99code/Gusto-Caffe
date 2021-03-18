bd2a07660e6ca2496f0f5ac98c105fe9aafac7d9
DanielM99code <danielmisiuk99@gmail.com>
2021-03-18 20:38:46 +0200

    h
    d

--- untitled6/settings.py
+++ untitled6/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://gustotest18032012.herokuapp.com/']
+ALLOWED_HOSTS = ['gustotest18032012.herokuapp.com']
 
 
 # Application definition
