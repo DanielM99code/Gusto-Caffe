1d559527cd041ed95b76cd90a5bb277a226b6dc9
DanielM99code <danielmisiuk99@gmail.com>
2021-03-19 18:00:10 +0200

    l

--- untitled6/settings.py
+++ untitled6/settings.py
@@ -143,12 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/Static/'
+STATIC_ROOT = '/untitled6/Static'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'Static'),
 )
 
 MEDIA_URL = '/media/'
